fe371709cb8473339c1cbd4d319c9cae351f57c2
Steve <steve0639@cchntek.com>
2022-05-10 14:21:19 +0800

    Updates

---
+++ precache-manifest.963975f72633a5a3660407bb1837cbfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eabf3c74083c75283b25b386d13fe0d",
+    "url": "/data_feedback/index.html"
+  },
+  {
+    "revision": "203cc707874f2f267f65",
+    "url": "/data_feedback/static/css/2.3a5f1a50.chunk.css"
+  },
+  {
+    "revision": "203cc707874f2f267f65",
+    "url": "/data_feedback/static/js/2.088268d5.chunk.js"
+  },
+  {
+    "revision": "8a66c768e98eb8510fa3fb866472b8a0",
+    "url": "/data_feedback/static/js/2.088268d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630219b9ea3fb0036a79",
+    "url": "/data_feedback/static/js/main.0fc24b5e.chunk.js"
+  },
+  {
+    "revision": "4172b7aeab04127852eb",
+    "url": "/data_feedback/static/js/runtime-main.3ea6cd0c.js"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/data_feedback/static/media/loader-big.a770b679.gif"
+  }
+]);
